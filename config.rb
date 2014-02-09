b2e3524dfb55044aef69be1c82bd19e03e5e9065
Sean Dillingham <sean@Sean-Dillinghams-MacBook-Pro-2.local>
2014-02-09 12:46:45 -0800

    refactor relevant JSON project data into local files using frontmatter; convery remaining top-level project data to YAML; remove proxy page method from config.rb

--- config.rb
+++ config.rb
@@ -21,12 +21,6 @@ activate :directory_indexes
 page "/",       :layout => :index_layout
 page "work/*",  :layout => :work_layout
 
-data.work.items.each do |project|
-  proxy "/work/#{project[:slug]}.html", "/work/project.html", :locals => { 
-    :item => project 
-  }, 
-  :ignore => true
-end
 
 # Per-page layout changes:
 #

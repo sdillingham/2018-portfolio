e657b69a4634325d6247e87231503459a83025f8
Sean Dillingham <sean.dillingham@gmail.com>
2014-03-29 17:15:33 -0700

    remove unneeded comments from config.rb

--- config.rb
+++ config.rb
@@ -11,14 +11,6 @@ activate :blog do |blog|
 end
 activate :directory_indexes
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
 
 ###
 # Page options, layouts, aliases and proxies
@@ -37,29 +29,9 @@ with_layout :blog_layout do
   end
 end
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
 ###
 # Helpers
 ###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
 # activate :livereload
@@ -118,6 +90,4 @@ configure :build do
   # Use relative URLs
   # activate :relative_assets
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end

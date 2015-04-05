7b1983b7c7a777d8f5246335afe3544d81437836
Sean Dillingham <sean.dillingham@gmail.com>
2015-04-05 14:56:03 -0700

    add SVG drawing animations

--- source/javascripts/body.js
+++ source/javascripts/body.js
@@ -2,5 +2,6 @@
 //= require waypoints/lib/jquery.waypoints.js
 //= require fluidbox/jquery.fluidbox.js
 //= require slick.js/slick/slick.js
+//= require vivus/dist/vivus.js
 
 //= require scripts.js

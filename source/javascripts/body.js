0b4f99b775529f2702cf6e3664eaec9b3809d8f8
Sean Dillingham <sean.dillingham@gmail.com>
2015-04-05 13:00:17 -0700

    Add Waypoints functionality to KPCC iPhone page
    * Adds waypoints components via Bower
    * Adds waypoint for lazy loading big BG images
    * Adds waypoint for lazy loading inline IMG
    * Adds waypoint for animating in UI controls

--- source/javascripts/body.js
+++ source/javascripts/body.js
@@ -1,5 +1,5 @@
 //= require jquery/jquery.min.js
-//= require picturefill/src/picturefill.js
+//= require waypoints/lib/jquery.waypoints.js
 //= require fluidbox/jquery.fluidbox.js
 //= require slick.js/slick/slick.js
 

3f97edd0a86d2e1578e8bf61073de384314be183
Sean Dillingham <sean.dillingham@gmail.com>
2015-06-06 11:13:05 -0700

    remove Vivus dependency from portfolio pages

--- source/javascripts/body.js
+++ source/javascripts/body.js
@@ -2,6 +2,5 @@
 //= require waypoints/lib/jquery.waypoints.js
 //= require fluidbox/jquery.fluidbox.js
 //= require slick.js/slick/slick.js
-//= require vivus/dist/vivus.js
 
 //= require scripts.js
